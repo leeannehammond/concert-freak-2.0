aafbb8c191450c32f266f5568b5a89837fdf5717
Zarela Graves <zarelavp@yahoo.com>
2018-09-18 00:57:10 -0400

    Moved react views to pages folder

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import Home from './components/Home'
-import Signup from './components/Signup'
+import Home from './pages/Home'
+import Signup from './pages/Signup'
 
 class App extends React.Component {
   render () {
